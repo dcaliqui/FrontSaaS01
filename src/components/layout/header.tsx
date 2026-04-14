f901bbdd3ee8b6e43fdb1d5a9d0e28955c2fc8fc
dcaliqui <damasiocipriano425@gmail.com>
2026-04-14 18:38:03 +0100

    Sqwaine

---
+++ src/components/layout/header.tsx
@@ -0,0 +1,52 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import icon from "@/assets/images/logo.png"
+
+interface NavLinkProps {
+    href: string;
+    children: React.ReactNode;
+    className?: string;
+}
+
+function NavLink({ href, children, className }: NavLinkProps) {
+    return (
+        <Link
+            href={href}
+            className={`
+        text-[#475569]
+        border-b-2 border-transparent
+        hover:text-[#4F46E5]
+        hover:border-[#4F46E5]
+        transition-all duration-300
+    
+        ${className}
+      `}
+        >
+            {children}
+        </Link>
+    )
+}
+
+export default function Header() {
+    return (
+        <>
+            <header className=' bg-white text-black py-4 border-b-[1px] border-zinc-200  shadow-2xl'>
+                <nav className='flex justify-between items-center container mx-auto' >
+
+                    <Image src={icon} alt='Logo' width={50} />
+
+                    <div className='flex gap-6'>
+                        <NavLink href='#'>Funcionalidades</NavLink>
+                        <NavLink href='#'>Preços</NavLink>
+                        <NavLink href='#'>Sobre</NavLink>
+                    </div>
+                    <div>
+                        <button id="button">
+                            Começar
+                        </button>
+                    </div>
+                </nav>
+            </header>
+        </>
+    )
+}
---
+++ src/components/layout/hero.tsx
@@ -0,0 +1,31 @@
+import Image from "next/image";
+import igreja from "@/assets/images/unnamed.png"
+
+export default function Hero() {
+    return (
+        <section className="bg-white">
+            <div className="container mx-auto grid grid-cols-1 md:grid-cols-2" >
+
+                <div className=" flex flex-col gap-6 justify-center space-y-8" >
+                    <h1 className="text-[#191C1E] text-6xl font-bold ">Modernize sua <br />administração <span className="text-[#2B3EA2]">com <br />graça.</span></h1>
+                    <h4 className="text-[#464555]">
+                        Simplifique o ministério para focar no que realmente
+                        importa: as pessoas. Uma plataforma completa desenhada
+                        para a igreja moderna.
+                    </h4>
+                    <div className="flex gap-6">
+                        <button id="button" >
+                            Começar
+                        </button>
+                        <button id="button1" >
+                            Ver demostração
+                        </button>
+                    </div>
+                </div>
+                <div className="bg-gray-200 items-center justify-center flex p-10 rounded-2xl mt-25 mb-10" >
+                    <Image src={igreja} alt="Igreja"  className="rotate-2 rounded-2xl" />
+                </div>
+            </div>
+        </section>
+    )
+}
