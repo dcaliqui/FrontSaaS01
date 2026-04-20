defa142d3ec878d0abc0f50678c0357286158970
dcaliqui <damasiocipriano425@gmail.com>
2026-04-20 19:42:32 +0100

    Responsividade..

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,18 +1,23 @@
+'use client'
+
 import Image from 'next/image'
 import Link from 'next/link'
 import icon from "@/assets/images/logo.png"
+import { Menu, X } from 'lucide-react';
+import { useState } from 'react';
+
 
 interface NavLinkProps {
-    href: string;
-    children: React.ReactNode;
-    className?: string;
+	href: string;
+	children: React.ReactNode;
+	className?: string;
 }
 
 function NavLink({ href, children, className }: NavLinkProps) {
-    return (
-        <Link
-            href={href}
-            className={`
+	return (
+		<Link
+			href={href}
+			className={`
         text-[#475569]
         border-b-2 border-transparent
         hover:text-[#4F46E5]
@@ -21,32 +26,41 @@ function NavLink({ href, children, className }: NavLinkProps) {
     
         ${className}
       `}
-        >
-            {children}
-        </Link>
-    )
+		>
+			{children}
+		</Link>
+	)
 }
 
+
 export default function Header() {
-    return (
-        <>
-            <header className=' bg-white text-black py-4 border-b-[1px] border-zinc-200  shadow-2xl '>
-                <nav className='flex justify-between items-center container mx-auto' >
 
-                    <Image src={icon} alt='Logo' width={50} />
+	return (
+		<>
 
-                    <div className='flex gap-6'>
-                        <NavLink href='#first'>Funcionalidades</NavLink>
-                        <NavLink href='#preco'>Preços</NavLink>
-                        <NavLink href='#third'>Sobre</NavLink>
-                    </div>
-                    <div>
-                        <button id="button">
-                            Começar
-                        </button>
-                    </div>
-                </nav>
-            </header>
-        </>
-    )
+			<header className=' bg-white text-black py-4 border-b-2 border-zinc-200  shadow-2xl '>
+				<nav className='flex justify-between items-center container mx-auto px-4 sm:px-0' >
+					<div className='flex justify-center items-center gap-4'>
+						<Image src={icon} alt='Logo' width={50} />
+						<h1 className=" sm:hidden text-2xl">CLARIS</h1>
+					</div>
+
+					<div className='hidden  sm:flex gap-6 '>
+						<NavLink href='#first'>Funcionalidades</NavLink>
+						<NavLink href='#preco'>Preços</NavLink>
+						<NavLink href='#third'>Sobre</NavLink>
+					</div>
+
+					<div className='flex flex-col'>
+						<button className='hidden sm:flex' id="button">
+							Começar
+						</button>
+					</div>
+				</nav>
+			</header>
+	
+
+			
+		</>
+	)
 }
