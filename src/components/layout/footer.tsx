c6189782830e9ead9b0a5aeed55236761c54b661
dcaliqui <damasiocipriano425@gmail.com>
2026-04-24 19:14:01 +0100

    Enviando..

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -7,7 +7,7 @@ export default function Footer() {
                     
                     {/* Logo + descrição */}
                     <div>
-                        <h2 className="text-[#2B3EA2] font-bold text-lg">
+                        <h2 className="text-[#1A365D] font-bold text-lg">
                             Claris
                         </h2>
                         <p className="text-gray-500 mt-3 text-sm leading-relaxed">
@@ -32,7 +32,7 @@ export default function Footer() {
 
                     {/* Social */}
                     <div>
-                        <h3 className="font-semibold text-gray-700 mb-3">
+                        <h3 className="font-semibold text-[#1A365D] mb-3">
                             Redes
                         </h3>
                         <ul className="space-y-2 text-sm text-gray-500">
@@ -45,7 +45,7 @@ export default function Footer() {
 
                     {/* Legal */}
                     <div>
-                        <h3 className="font-semibold text-gray-700 mb-3">
+                        <h3 className="font-semibold text-[#1A365D] mb-3">
                             Legal
                         </h3>
                         <ul className="space-y-2 text-sm text-gray-500">
--- src/components/layout/price.tsx
+++ src/components/layout/price.tsx
@@ -14,64 +14,77 @@ export default function Prices() {
 						Um plano para cada momento da sua congregação.
 					</h2>
 					<div className="flex justify-between  w-full">
-						<div className="flex gap-4 justify-between w-full">
-							<div className="bg-[#F3F3F3] flex flex-col gap-4 rounded-2xl space-y-3 p-10" >
+						<div className="flex gap-4 justify-between w-full items-center">
+							<div className="bg-[#F3F3F3] flex flex-col gap-4 rounded-2xl  h-120 space-y-3 p-5 w-120" >
 								<p className="text-[#1A365D]  font-bold text-[20px]">Seed</p>
 								<p className="text-[#1A365D] font-bold text-[40px] ">R$ 149 <span className="text-[#64748B] text-[24px]">/mês</span></p>
-								<div className="flex flex-col gap-3 space-y-2">
-									<div className="flex gap-3 justify-center items-center">
+								<div className="flex flex-col gap-3 space-y-2 p-2">
+									<div className="flex gap-3 ">
 										<Check size={22} className="text-amber-500 font-bold" />
 										<p className="text-[#475569] text-[24px]">Até 100 Membros</p>
 									</div>
-									<div className="flex gap-3 justify-center items-center">
+									<div className="flex gap-3 ">
 										<Check size={22} className="text-amber-500 font-bold" />
 										<p className="text-[#475569] text-[24px]">Gestão de Eventos</p>
 									</div>
-									<div className="flex gap-3 justify-center items-center">
+									<div className="flex gap-3 ">
 										<Check size={22} className="text-amber-500 font-bold" />
 										<p className="text-[#475569] text-[24px]">Suporte via Chat</p>
 									</div>
-
+								</div>
+								<div className="flex items-center justify-center p-4">
+									<button className="p-3 border-solid border-[1px] border-[#002045] rounded-2xl text-[#002045] font-bold cursor-pointer hover:bg-[#002045] hover:text-white transition-discrete duration-500 w-full">
+										Começar Agora
+									</button>
 								</div>
 							</div>
 
-							<div className="bg-[#002045] flex flex-col gap-4 rounded-2xl space-y-3 p-10 h-100" >
+							<div className="bg-[#002045] flex flex-col gap-4 rounded-2xl space-y-3 p-10 h-130 w-150" >
 								<p className="text-white  font-bold text-[20px]">Growth</p>
 								<p className="text-white font-bold text-[40px] ">R$ 149 <span className="text-[#64748B] text-[24px]">/mês</span></p>
 								<div className="flex flex-col gap-3 space-y-2">
-									<div className="flex gap-3 justify-center items-center">
+									<div className="flex gap-3 ">
 										<Check size={22} className="text-amber-500 font-bold" />
 										<p className="text-white text-[24px]">Membros Ilimitados</p>
 									</div>
-									<div className="flex gap-3 justify-center items-center">
+									<div className="flex gap-3 ">
 										<Check size={22} className="text-amber-500 font-bold" />
 										<p className="text-white text-[24px]">Múltiplos Campus</p>
 									</div>
-									<div className="flex gap-3 justify-center items-center">
+									<div className="flex gap-3 ">
 										<Check size={22} className="text-amber-500 font-bold" />
 										<p className="text-white text-[24px]">Relatórios Financeiros</p>
 									</div>
 
 								</div>
+								<div className="flex items-center justify-center p-4">
+									<button className="p-5 border-solid border-[1px] bg-[#FFDEA5] border-[#261900] rounded-2xl  font-bold cursor-pointer text-[#261900]  w-full">
+										Começar Agora
+									</button>
+								</div>
 							</div>
 
-							<div className="bg-[#F3F3F3] flex flex-col gap-4 rounded-2xl space-y-3 p-10" >
+							<div className="bg-[#F3F3F3] flex flex-col gap-4 rounded-2xl   h-120 space-y-3 p-5 w-120" >
 								<p className="text-[#1A365D]  font-bold text-[20px]">Seed</p>
 								<p className="text-[#1A365D] font-bold text-[40px] ">R$ 149 <span className="text-[#64748B] text-[24px]">/mês</span></p>
-								<div className="flex flex-col gap-3 space-y-2">
-									<div className="flex gap-3 justify-center items-center">
+								<div className="flex flex-col gap-3 space-y-2 p-2">
+									<div className="flex gap-3 ">
 										<Check size={22} className="text-amber-500 font-bold" />
 										<p className="text-[#475569] text-[24px]">Até 100 Membros</p>
 									</div>
-									<div className="flex gap-3 justify-center items-center">
+									<div className="flex gap-3 ">
 										<Check size={22} className="text-amber-500 font-bold" />
 										<p className="text-[#475569] text-[24px]">Gestão de Eventos</p>
 									</div>
-									<div className="flex gap-3 justify-center items-center">
+									<div className="flex gap-3 ">
 										<Check size={22} className="text-amber-500 font-bold" />
 										<p className="text-[#475569] text-[24px]">Suporte via Chat</p>
 									</div>
-
+								</div>
+								<div className="flex items-center justify-center p-4">
+									<button className="p-3 border-solid border-[1px] border-[#002045] rounded-2xl text-[#002045] font-bold cursor-pointer hover:bg-[#002045] hover:text-white transition-discrete duration-500 w-full">
+										Começar Agora
+									</button>
 								</div>
 							</div>
 						</div>
