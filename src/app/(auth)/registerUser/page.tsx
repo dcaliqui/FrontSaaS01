692429eeb5c3f83b7aa06d03aece0a5a75172604
dcaliqui <damasiocipriano425@gmail.com>
2026-04-29 09:16:07 +0100

    Fazendo o resgistar o user..

--- src/app/(auth)/registerUser/page.tsx
+++ src/app/(auth)/registerUser/page.tsx
@@ -15,37 +15,41 @@ export default function Login() {
 							<p className="text-white text-4xl">CLARIS</p>
 						</div>
 						<div className="flex flex-col gap-3 items-center justify-center">
-							<p className="text-[#FFDEA5] tracking-wide text-[30px]">Escrituras para hoje</p>
-							<p className="italic text-white   text-[50px] text-center font-serif">"Que tudo seja feito
-								com decência e ordem”
-							</p>
-							<p className="text-[#86A0CD] text-[24px]">— 1 Coríntios 14:40</p>
+							<p className="text-[#FFDEA5] tracking-wide font-bold text-[14px]">Um novo amanhecer</p>
+							<p className="italic text-white   text-[60px] text-center font-serif mb-2">"Comece a sua jornada sagrada.”</p>
+							<p className="text-[#86A0CD] text-[18px] text-center">Junte-se a uma comunidade global dedicada à reflexão silenciosa, ao ministério digital e ao crescimento espiritual.</p>
 						</div>
 						<div className="flex justify-around text-white">
 						</div>
 					</div>
-					<div className="w-full  h-full flex flex-col justify-center items-center gradientes">
+					<div className="w-full  h-full flex flex-col py-20 items-center gradientes">
 						<div className="w-159 h-181 flex flex-col">
-							<p className="text-3xl text-black font-serif italic mb-4">Comece sua jornada de gestão.</p>
-							<p className="text-[#43474E] mb-12">Crie uma conta administrativa para gerenciar a presença digital da sua igreja.</p>
-							<div className="flex gap-3 mb-6">
-								<p className="bg-[#FFDEA5] text-[#1A1C1C] px-4 rounded-2xl"> 1</p>
-								<p className="text-[#002045]">Dados pessoais</p>
+							<p className="text-[36px] text-[#002045] font-serif italic mb-4">Criar uma conta.</p>
+							<p className="text-[#43474E] text-[16px] mb-12">Entre no Santuário Digital. O seu caminho começa aqui.</p>
+							<button className="p-4 bg-white text-black flex gap-2 hover:cursor-pointer items-center justify-center rounded-2xl mb-10">
+								<Image src={Google} alt="Google" className="w-5 h-5" />
+								<p>Sign up with Google</p>
+							</button>
+							<div className="flex gap-3 items-center justify-between mb-8">
+								<div className="w-54 h-px bg-gray-400"></div>
+								<p className="text-gray-500 text-[12px]">OR USE EMAIL</p>
+								<div className="w-54 h-px bg-gray-400"></div>
 							</div>
 							<div className="flex ">
 								<form action="" className="flex flex-col  w-full">
-									<div className="flex justify-between gap-4 ">
-										<div className="flex flex-col w-[50%]">
+									<div className="flex flex-col justify-between gap-4 ">
+										<div className="flex flex-col">
 											<label htmlFor="fullNmae" className="mb-1 text-[#43474E]">Nome completo</label>
 											<input type="text" id="fullNmae" placeholder="ex. Delson Pedro" className="text-[#6B7280]  bg-white rounded-2xl h-13.5 p-4" />
+											<label htmlFor="fullNmae" className="mb-1 text-[#43474E]">E-mail</label>
+											<input type="text" id="fullNmae" placeholder="ex. delsonpedro@church.com" className="text-[#6B7280]  bg-white rounded-2xl h-13.5 p-4 mb-1" />
 											<label htmlFor="pass" className="mb-1 text-[#43474E]">Senha</label>
 											<input type="password" id="pss" placeholder="........" className="text-[#74777F] bg-white rounded-2xl h-13.5 p-4 mb-1" />
 											<label htmlFor="passC" className="mb-1 text-[#43474E]">Confirmar senha</label>
 											<input type="password" id="pssC" placeholder="........" className="text-[#74777F] bg-white rounded-2xl h-13.5 p-4 mb-10" />
 										</div>
-										<div className="flex flex-col w-[50%]">
-											<label htmlFor="fullNmae" className="mb-1 text-[#43474E]">E-mail</label>
-											<input type="text" id="fullNmae" placeholder="ex. delsonpedro@church.com" className="text-[#6B7280]  bg-white rounded-2xl h-13.5 p-4 mb-1" />
+										<div className="flex flex-col ">
+
 											<label htmlFor="tel" className="mb-1 text-[#43474E]">Telefone</label>
 											<input type="password" id="tel" placeholder="9xx xxx xxx" className="text-[#74777F] bg-white rounded-2xl h-13.5 p-4 mb-1" />
 											<label htmlFor="photo" className="mb-1 text-[#43474E]">Foto</label>
