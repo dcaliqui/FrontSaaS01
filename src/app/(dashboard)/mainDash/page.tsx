2f7a1c5a65ef8c41000aac88fdc01b43c9bba91c
dcaliqui <damasiocipriano425@gmail.com>
2026-05-05 01:19:57 +0100

    MainDash

--- src/app/(dashboard)/mainDash/page.tsx
+++ src/app/(dashboard)/mainDash/page.tsx
@@ -1,7 +1,10 @@
+"use client"
 import Image from 'next/image'
 import icon from "@/assets/images/logoSem.png"
 import Link from 'next/link'
 import { Search, Bell, Settings } from "lucide-react";
+import CommunityCard from '@/components/layout/commityCard';
+import heroImg from "@/assets/images/create.png";
 
 interface NavLinkProps {
 	href: string;
@@ -75,7 +78,48 @@ export default function MainDash() {
 								<button className='bg-[#1E3A8A] text-white rounded-2xl px-4 py-2 ml-2'>Pesquisar</button>
 							</div>
 						</div>
+						<div className='flex mt-10 items-center justify-center'>
+							<p className='text-[#002045]  w-40 mr-1'>MINHAS IGREJAS</p>
+							<div className='h-px bg-zinc-400 w-full'></div>
+						</div>
+						<div className='flex overflow-scroll gap-8 py-3'>
+							<CommunityCard name='Igreja Batista' description='Uma comunidade de fé e amor' logoUrl={null} membersCount={120} responsable='Pastor João' onClick={() => { }} />
+							<CommunityCard name='Igreja Católica' description='Uma comunidade de fé e amor' logoUrl={null} membersCount={120} responsable='Pastor João' onClick={() => { }} />
+							<CommunityCard name='Igreja Adventista' description='Uma comunidade de fé e amor' logoUrl={null} membersCount={120} responsable='Pastor João' onClick={() => { }} />
+							<CommunityCard name='Igreja Presbiteriana' description='Uma comunidade de fé e amor' logoUrl={null} membersCount={120} responsable='Pastor João' onClick={() => { }} />
+						</div>
+						<section
+							style={{
+								backgroundImage: `linear-gradient(
+                   to right, 
+				    rgba(26, 54, 93, 0.98) 10%, 
+				    rgba(26, 54, 93, 0.80) 50%, 
+				    rgba(26, 54, 93, 0.60) 100%
+                ), url(${heroImg.src})`
+							}}
+							className="bg-white bg-center bg-cover mt-10 mb-10 rounded-2xl flex flex-col justify-between p-20"
+						>
+							<div className='flex flex-col'>
+							<p className='text-white text-4xl italic'>Manifesta a tua visão</p>
+							<p className='text-[#DBEAFE] w-120 mt-4 '>
+								Crie a sua igreja e comece hoje a construir uma comunidade de fé vibrante
+								e acolhedora, capacitada com ferramentas que organizam, conectam e fortalecem a comunhão.
+								‘Pois onde dois ou três estiverem reunidos em meu nome, ali estou no meio deles.
+								(Mateus 18:20)
+							</p>
+							</div>
+							<button className='bg-[#FFDEA5] text-[#261900]  w-60  rounded-2xl px-4 py-3 mt-12 cursor-pointer'>Criar Igreja</button>
+						</section>
+						<div className='flex justify-between items-center'>
+							<p className='text-[#002045]  w-40 mr-1'>DESCUBRIR IGREJAS</p>
+							<div className='h-px bg-zinc-400 w-full'></div>
+						</div>
+						<div className='flex overflow-scroll gap-8 py-3'>
+							
+						</div>
+
 					</nav>
+
 				</div>
 			</div>
 		</div>
---
+++ src/components/layout/commityCard.tsx
@@ -0,0 +1,52 @@
+import { ArrowRight } from "lucide-react";
+
+
+interface CommunityCardProps {
+  name: string;
+  description: string;
+  logoUrl: string | null;
+  membersCount: number;
+  responsable: string;
+  onClick: () => void;
+  className?: string;
+
+}
+
+
+export default function CommunityCard({
+  name,
+  description,
+  logoUrl,
+  membersCount,
+  responsable,
+  onClick,
+  className = '',
+}: CommunityCardProps) {
+  return (
+    <div className="bg-white w-148 h-63.5 rounded-lg shadow-md  cursor-pointer flex" onClick={onClick}>
+      <div className="w-73.75 ">
+        {logoUrl ? (
+          <img src={logoUrl} alt={`${name} logo`} className="w-full h-full object-cover rounded-lg" />
+        ) : (
+          <div className="w-73.75 h-full bg-gray-200 flex items-center justify-center rounded-lg">
+            <span className="text-gray-500">No Logo</span>
+          </div>
+        )}
+      </div>
+      <div className="w-73.75  flex flex-col p-8 justify-between">
+        <div className="flex flex-col">
+          <p className="text-sm text-[#1A1C1C] uppercase tracking-wide text-[10px]">{responsable}</p>
+          <h3 className="text-lg font-bold text-[#002045] text-[24px]">{name}</h3>
+          <p className="text-[#475F83] text-[14px]">{description}</p>
+        </div>
+        <div className="flex justify-between items-center mt-6">
+          <span className="text-sm text-gray-500">{membersCount} members</span>
+          <button className="flex items-center text-[#1E3A8A] p-2 hover:rounded-2xl cursor-pointer   hover:bg-[#1E3A8A] hover:text-white" onClick={onClick}>
+            <span className="text-sm ">Entrar</span>
+            <ArrowRight size={16} className=" ml-1" />
+          </button>
+        </div>
+      </div>
+    </div>
+  )
+}
