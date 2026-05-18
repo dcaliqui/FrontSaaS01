0cce9e9af3659c6dcc55ce9ed3227d504442d1ba
dcaliqui <damasiocipriano425@gmail.com>
2026-05-18 20:33:54 +0100

    Começando a página de entrar em cada igreja

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,7 +17,7 @@ export default function LandingPage() {
 	return (
 		<div className="h-screen flex flex-col ">
 			<Header />
-{/* 			{
+			{/* 			{
 				isAuthenticated ? user?.displayName : "Loading..." 
 			} */}
 			<div className="flex-1 flex flex-col overflow-auto">
