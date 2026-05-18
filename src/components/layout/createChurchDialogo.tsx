04e72b1c9ac02ec974ef81cbcdbda09cb76abb40
dcaliqui <damasiocipriano425@gmail.com>
2026-05-18 08:12:12 +0100

    feat: add Radix UI components and enhance create church dialog with organization selection

--- src/components/layout/createChurchDialogo.tsx
+++ src/components/layout/createChurchDialogo.tsx
@@ -12,33 +12,30 @@ import {
 import { Field, FieldGroup } from "@/components/ui/field"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
+import OrganizatioSelect from "./organizatioSelect"
 
 export function DialogDemo() {
   return (
     <Dialog >
       <form>
         <DialogTrigger asChild className="bg-[#FFDEA5] text-[#261900] w-60 rounded-2xl px-9 py-6 mt-12 hover:cursor-pointer">
-          <Button variant="outline" >Criar igreja</Button>
+          <Button variant="outline" >Open Dialog</Button>
         </DialogTrigger>
-        <DialogContent className="w-130 sm:max-w-7xl md:max-w-6xl lg:max-w-7xl max-h-[90vh] p-8 ">
+        <DialogContent className="w-full sm:max-w-7xl md:max-w-6xl lg:max-w-7xl max-h-[90vh] p-8 overflow-auto">
           <DialogHeader>
             <DialogTitle>Criar Igreja</DialogTitle>
             <DialogDescription>
               Preencha os detalhes da sua igreja aqui. Clique em salvar quando terminar.
             </DialogDescription>
           </DialogHeader>
-          <FieldGroup className="w-120">
-            <Field className="w-110">
-              <Label htmlFor="name-1">Name da igreja</Label>
-              <Input id="name-1"  placeholder="Igreja de cristo" />
+          <FieldGroup>
+            <Field>
+              <Label htmlFor="name-1">Name</Label>
+              <Input id="name-1" name="name" defaultValue="Pedro Duarte" />
             </Field>
-            <Field className="w-100">
-              <Label >Localização</Label>
-              <Input id="local" placeholder="Província, Município e bairro"  />
-            </Field>
-			<Field className="w-110">
-              <Label >Descrição</Label>
-              <textarea name="description" id="" placeholder="Descreva sua igreja..." className="w-20 min-h-20 max-h-21 focus:outline-none border border-gray-300] p-1" />
+            <Field>
+              <Label htmlFor="username-1">Username</Label>
+              <Input id="username-1" name="username" defaultValue="@peduarte" />
             </Field>
           </FieldGroup>
           <DialogFooter className="w-110">
