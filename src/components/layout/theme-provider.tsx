c6ed220438572552a263e6575b0d1b908519dcb1
dcaliqui <damasiocipriano425@gmail.com>
2026-06-04 14:36:30 +0100

    refactor: optimize theme provider configuration and clean up global font imports

--- src/components/layout/theme-provider.tsx
+++ src/components/layout/theme-provider.tsx
@@ -5,7 +5,13 @@ import { ThemeProvider as NextThemesProvider } from "next-themes"
 
 export function ThemeProvider({ children }: { children: React.ReactNode }) {
   return (
-    <NextThemesProvider attribute="class" defaultTheme="system" enableSystem>
+    <NextThemesProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      enableColorScheme={false}
+      disableTransitionOnChange
+    >
       {children}
     </NextThemesProvider>
   )
