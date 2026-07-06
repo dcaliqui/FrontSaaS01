f17b3cd9be3a9702dbacfce11e04b92cca1f8513
Costantino Gouveia <costantino.gouveia938@gmail.com>
2026-07-06 15:47:32 +0100

    update

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+import Header from "../components/layout/header";
+import Hero from "../components/layout/hero";
+
+
+
+export default function LandingPage()
+{
+  return (
+    <>
+        <Header />
+        <Hero/>
+    </>
+
+  )
+}
